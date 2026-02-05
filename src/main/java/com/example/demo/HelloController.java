23c6cbd2ad458f020308b4ea2b5186c2cbce7c3f
eisjak01 <eisjak01@gasos-ro.cz>
2026-02-05 10:28:08 +0100

    e

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,29 +1,51 @@
 package com.example.demo;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.GridPane;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class HelloController {
     private Card firstCard;
     private Card secondCard;
     private ArrayList<Card> cards =  new ArrayList<>();
-    private int score;
-
+    Player firstPlayer = new Player("First", true);
+    Player secondPlayer = new Player("Second", false);
     @FXML
-    private Label scoreLabel;
+    private Label PlayerOneScoreLabel, PlayerTwoScoreLabel, PlayerTurn, InfoLabel;
     @FXML
     private GridPane grid;
+    @FXML
+    private Button NewGameButton;
 
     @FXML
     private void initialize() {
-        Player firstPlayer = new Player("First");
-        Player secondPlayer = new Player("Second");
+        int sc = 0;
         generateCards();
         displayCards();
+        newGame();
+    }
+    @FXML
+    private void newGame(){
+        for(Card card : cards){
+            card.flipBack();
+            card.getButton().setDisable(false);
+        }
+        firstPlayer.setScore(0);
+        secondPlayer.setScore(0);
+        InfoLabel.setText("");
+        PlayerOneScoreLabel.setText("PLAYER ONE SCORE: " + firstPlayer.getScore());
+        PlayerTwoScoreLabel.setText("PLAYER TWO SCORE: " + secondPlayer.getScore());
+        if (firstPlayer.isTurn()) {
+            PlayerTurn.setText("TURN: PLAYER ONE");
+        }else{
+            PlayerTurn.setText("TURN: PLAYER TWO");
+        }
+        NewGameButton.setDisable(true);
     }
 
     @FXML
@@ -36,6 +58,7 @@ public class HelloController {
 
     @FXML
     private void displayCards(){
+        Collections.shuffle(cards);
         int column = 0;
         int row = 0;
         for(int i = 0; i < cards.size(); i++){
@@ -64,25 +87,81 @@ public class HelloController {
     @FXML
     private void checkMatch() {
         if (firstCard.getId() == secondCard.getId()){
-            firstCard.getButton().setDisable(true);
-            secondCard.getButton().setDisable(true);
-            firstCard = null;
-            secondCard = null;
-            score++;
-            scoreLabel.setText("SCORE: " + score);
+            if (firstPlayer.isTurn()) {
+                InfoLabel.setText("PLAYER ONE GOT A POINT");
+                firstCard.getButton().setDisable(true);
+                secondCard.getButton().setDisable(true);
+                firstCard = null;
+                secondCard = null;
+                firstPlayer.setScore(firstPlayer.getScore() + 1);
+                PlayerOneScoreLabel.setText("PLAYER ONE SCORE: " + firstPlayer.getScore());
+                firstPlayer.setTurn(false);
+                secondPlayer.setTurn(true);
+                PlayerTurn.setText("TURN: PLAYER TWO");
+                System.out.print(firstPlayer.getScore()+secondPlayer.getScore());
+                checkGameState();
+            }else{
+                InfoLabel.setText("PLAYER TWO GOT A POINT");
+                firstCard.getButton().setDisable(true);
+                secondCard.getButton().setDisable(true);
+                firstCard = null;
+                secondCard = null;
+                secondPlayer.setScore(secondPlayer.getScore() + 1);
+                PlayerTwoScoreLabel.setText("PLAYER TWO SCORE: " + secondPlayer.getScore());
+                secondPlayer.setTurn(false);
+                firstPlayer.setTurn(true);
+                PlayerTurn.setText("TURN: PLAYER ONE");
+                System.out.print(firstPlayer.getScore()+secondPlayer.getScore());
+                checkGameState();
+            }
         }else{
-            firstCard.flipBack();
-            secondCard.flipBack();
-            firstCard = null;
-            secondCard = null;
+            if(firstPlayer.isTurn()){
+                InfoLabel.setText("WRONG CARD");
+                firstCard.flipBack();
+                secondCard.flipBack();
+                firstCard = null;
+                secondCard = null;
+                firstPlayer.setTurn(false);
+                secondPlayer.setTurn(true);
+                PlayerTurn.setText("TURN: PLAYER TWO");
+                System.out.print(firstPlayer.getScore()+secondPlayer.getScore());
+            }else{
+                InfoLabel.setText("WRONG CARD");
+                firstCard.flipBack();
+                secondCard.flipBack();
+                firstCard = null;
+                secondCard = null;
+                secondPlayer.setTurn(false);
+                firstPlayer.setTurn(true);
+                PlayerTurn.setText("TURN: PLAYER ONE");
+                System.out.print(firstPlayer.getScore()+secondPlayer.getScore());
+            }
+        }
+    }
+    @FXML
+    private void checkWinner() {
+        if (firstPlayer.getScore() > secondPlayer.getScore()){
+            InfoLabel.setText("PLAYER ONE WON (Score: "+firstPlayer.getScore()+")");
+        }else if (secondPlayer.getScore() > firstPlayer.getScore()){
+            InfoLabel.setText("PLAYER TWO WON (Score: "+secondPlayer.getScore()+")");
+        }
+    }
+    @FXML
+    private void checkGameState() {
+        if (firstPlayer.getScore()+secondPlayer.getScore() == 8){
+            checkWinner();
+            NewGameButton.setDisable(false);
+
         }
     }
 
     public ArrayList<Card> getCards() {
+
         return cards;
     }
 
     public void setCards(ArrayList<Card> cards) {
         this.cards = cards;
     }
+
 }
--- src/main/java/com/example/demo/Player.java
+++ src/main/java/com/example/demo/Player.java
@@ -3,7 +3,27 @@ package com.example.demo;
 public class Player {
     private String name;
     private int score;
-    Player(String name) {
+    private boolean turn;
+    Player(String name, boolean turn) {
         this.name = name;
+        this.turn = turn;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getScore() {
+        return score;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public boolean isTurn() {
+        return turn;
+    }
+    public void setTurn(boolean turn) {
+        this.turn = turn;
     }
 }
